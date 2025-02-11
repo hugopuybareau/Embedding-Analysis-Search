8a72ba29bdad59baf0a946c4f3fa762d6d75b477
hugopuybareau <hugopuybareau@github.com>
2025-02-11 15:47:41 +0100

    transform & similarities explained

--- embedding_models.py
+++ embedding_models.py
@@ -25,13 +25,40 @@ class EmbeddingModel:
                 sentences=tokenized_texts,
                 vector_size=100,
                 window=5,
-                min_count=1
+                min_count=0
             )
         
         elif self.method == 'sbert':
             self.model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
-        else:
-            raise ValueError('Unknown method -> (word2vec, tfidf, sbert)')
+            
+        else: raise ValueError('Unknown method -> (word2vec, tfidf, sbert)')
+            
+        
+    def transform(self, texts):
+        if self.method == 'tfidf':
+            return self.model.transform(texts).toarray()
+        
+        elif self.method == 'word2vec':
+            tokenized_texts = [word_tokenize(text.lower()) for text in texts]
+            return np.array([
+                np.mean([self.model.wv[word] for word in words if word in self.model.wv] or [np.zeros(100)], axis=0)
+                for words in tokenized_texts
+            ])
+        
+        # TF-IDF and Word2Vec won't have the same shape because TF-IDF creates a sparce matrix shaped 
+        # (number of documents, number of words)
+        # SBERT will be 384 because "paraphrase-MiniLM-L6-v2" is like that. 
+        # For example, paraphrase-mpnet-base-v2 would be 768, more accurate but slower. 
+
+        elif self.method == 'sbert':
+            return self.model.encode(texts)
+        
+        else: raise ValueError('Unknown method -> (word2vec, tfidf, sbert)')
+
+    def similarity(self, vec1, vec2):
+        return cosine_similarity([vec1], [vec2])[0][0]
+
+
         
 
 
